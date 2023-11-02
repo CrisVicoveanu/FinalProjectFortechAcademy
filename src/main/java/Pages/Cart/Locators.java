1c5ce24413e1dc7efdcd40de8f5207291438b12e
Cristina Vicoveanu <cris.vicoveanu@gmail.com>
2023-11-02 11:33:54 +0200

    first commit

---
+++ src/main/java/Pages/Cart/Locators.java
@@ -0,0 +1,7 @@
+package Pages.Cart;
+
+public class Locators {
+    protected static final String PRODUCTNAMELOCATOR = "div.product-line-grid-body.col-md-5.col-7 > div:nth-child(1) > a";
+    protected static final String CARTPRODUCTQUANTITYLOCATOR = "div.col-md-6.col-6.qty > div > div > input";
+
+}
